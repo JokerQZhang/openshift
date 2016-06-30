6dfcf65f4a65919efc7ceaeffc5df8e914a02768
JokerQZhang <383913397@qq.com>
2016-06-30 21:40:37 +0800

    first

---
+++ src/main/java/com/joker/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.joker.service.impl;
+
+import com.joker.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
